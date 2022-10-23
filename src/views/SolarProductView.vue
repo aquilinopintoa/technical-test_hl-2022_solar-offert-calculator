e431c8a48582f82cec561c6099272ee1c92d5450
aquilinopintoa <aquilinopintoa@gmail.com>
2022-10-23 19:38:56 +0200

    feat(init): init repo

---
+++ src/views/SolarProductView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <pre>{{ clientJson }}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import clientJson from "../data/clients.json";
+
+export default defineComponent({
+  setup() {
+    return { clientJson };
+  },
+});
+</script>
+
+<style scoped></style>
